de5b25b62ca41ae9d030a6f1815b8a57960c6971
Chris Gauthier <cpgauthier@gmail.com>
2012-05-01 16:03:57 -0400

    Map now uses a master list and an active list, with "lazy" updates.

--- map.js
+++ map.js
@@ -12,10 +12,11 @@ GAS.map = {
 	MAP_HEIGHT: 300,
 	EYE_RADIUS: 200,
 	
-	node: [],
+	master: [],
+	active: [],
 	
 	updateIndex: 0,
-	updateLength: 500,
+	updateLength: 400,
 	
 	dir: SOAR.vector.create(),
 
@@ -34,57 +35,86 @@ GAS.map = {
 			x = GAS.random(-this.MAP_RADIUS, this.MAP_RADIUS);
 			y = GAS.random(-this.MAP_HEIGHT, this.MAP_HEIGHT);
 			z = GAS.random(-this.MAP_RADIUS, this.MAP_RADIUS);
-			this.node.push( {
+			this.master.push( {
 				object: GAS.weeds.create(x, y, z),
 				center: SOAR.vector.create(x, y, z),
-				radius: GAS.weeds.BASE_RADIUS,
-				active: false
+				radius: GAS.weeds.BASE_RADIUS
 			} );
 		}
 
 		// generate spice object for test
-/*		this.node.push( {
+/*		this.master.push( {
 			object: GAS.spice.create(0, 0, -25),
 			center: SOAR.vector.create(0, 0, -25),
-			radius: GAS.spice.CLOUD_RADIUS,
-			active: false
+			radius: GAS.spice.CLOUD_RADIUS
 		} );
 */
 
 		// generate paddler for test
 		var p = GAS.paddler.create();
-		p.position.set(0, 0, -10);
-		this.node.push( {
+		p.position.set(0, 0, -75);
+		this.master.push( {
 			object: p,
-			center: SOAR.vector.create(0, 0, -10),
-			radius: GAS.paddler.COMFORT_ZONE,
-			active: false
+			center: SOAR.vector.create(0, 0, -75),
+			radius: GAS.paddler.COMFORT_ZONE
 		} );
 	},
 	
 	/**
-		update the map nodes active status
+		update the list of active map nodes
 		
 		@method update
 	**/
 	
 	update: function() {
-		var r = this.EYE_RADIUS;
-		var i = this.updateIndex;
-		var l = this.node.length;
-		var c = this.updateLength;
 		var p = GAS.player.camera.position;
-		
-		while (c) {
-			var n = this.node[i];
-			n.active = n.center.distance(p) < r + n.radius;
+		var r = this.EYE_RADIUS;
+		var b = false;
+		var i, il, j, c, n;
+
+		// search through the next segment of the master list
+		// for anything newly visible & add it to active list
+		i = this.updateIndex;
+		il = this.master.length;
+		for (c = this.updateLength; c; ) {
+			n = this.master[i];
+			if (!n.active && (n.center.distance(p) <= r + n.radius)) {
+				n.active = true;
+				this.active.push(n);
+			}
 			c--;
-			if (++i >= l) {
+			if (++i >= il) {
 				i = 0;
+				b = true;
 			}
 		}
-		
 		this.updateIndex = i;
+
+		// if we've completed a full refresh cycle, do some housecleaning
+		if (b) {
+		
+			// remove anything from the active list that's out of sight
+			for (i = 0, il = this.active.length; i < il; i++) {
+				n = this.active[i];
+				if (n && (n.center.distance(p) > r + n.radius)) {
+					n.active = false;
+					delete this.active[i];
+				}
+			}
+			
+			// close any gaps in the active list
+			// (object will sort before undefined)
+			GAS.map.active.sort();
+			// find the first undefined, and chop the array off there
+			for (i = 0; i < il; i++) {
+				if (!this.active[i]) {
+					break;
+				}
+			}
+			this.active.length = i;
+		}
+
+//		GAS.hud.debug(this.active.length);
 	},
 	
 	/**
@@ -103,10 +133,10 @@ GAS.map = {
 		// reset last draw
 		this.lastDraw = "";
 		
-		// iterate through all nodes
-		for (i = 0, il = this.node.length; i < il; i++) {
-			n = this.node[i];
-			if (n.active) {
+		// iterate through active nodes
+		for (i = 0, il = this.active.length; i < il; i++) {
+			n = this.active[i];
+			if (n) {
 				dir.copy(n.center).sub(cp);
 				if (dir.length() < 100) {
 					n.object.draw();
@@ -120,7 +150,7 @@ GAS.map = {
 				}
 			}
 		}
-//		GAS.hud.debug(c);
+		GAS.hud.debug(c);
 	}
 
 };
